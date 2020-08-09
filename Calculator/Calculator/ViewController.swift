dd54418f21dc66f2af732de4315161690282651e
Maciej Jastrzebski <maciej.jastrzebski@partners.mbank.pl>
2020-08-09 17:12:55 +0200

    changes

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Maciej Jastrzębski on 8/9/20.
+//  Copyright © 2020 SKY.NET. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func touchDigit(_ sender: UIButton) {
+        print("7777777")
+    }
+    
+
+}
