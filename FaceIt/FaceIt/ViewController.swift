e1f1d4c9b806b0016fe34f9bd561bbd92228fc8d
Maciej Jastrzebski <maciej.jastrzebski@partners.mbank.pl>
2020-08-14 11:57:40 +0200

    Lesson 4: Views

---
+++ FaceIt/FaceIt/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Maciej Jastrzębski on 8/13/20.
+//  Copyright © 2020 SKY.NET. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+}
+
